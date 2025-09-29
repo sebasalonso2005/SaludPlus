8c7453e3ef6dbf2d98ecc9bfa8220ffa7ea43197
sebasalonso2005 <150994643+sebasalonso2005@users.noreply.github.com>
2025-09-29 15:28:29 -0500

    Update Usuarios.java

--- src/main/java/pe/edu/sp/demosaludplus/Entities/Usuarios.java
+++ src/main/java/pe/edu/sp/demosaludplus/Entities/Usuarios.java
@@ -1,1 +1,143 @@
+package pe.edu.sp.demosaludplus.Entities;
+
+import jakarta.persistence.*;
+
+import java.util.HashSet;
+import java.util.Set
+
+@Entity
+@Table(
+        name = "usuarios",
+        uniqueConstraints = {
+                @UniqueConstraint(name = "uk_usuarios_dni",    columnNames = "dni"),
+                @UniqueConstraint(name = "uk_usuarios_correo", columnNames = "correo"),
+                @UniqueConstraint(name = "uk_usuarios_user",   columnNames = "username")
+        }
+)
+public class Usuarios {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id_usuario")
+    private Integer idUsuario;
+
+    @Column(name = "nombre", nullable = false, length = 100)
+    private String nombre;
+
+    @Column(name = "dni", nullable = false, length = 20)
+    private String dni;
+
+    @Column(name = "correo", nullable = false, length = 100)
+    private String correo;
+
+    @Column(name = "telefono", nullable = false, length = 100)
+    private String telefono;
+
+    @Column(name = "direccion", nullable = false, length = 200)
+    private String direccion;
+
+    @Column(name = "comunidad", nullable = false, length = 100)
+    private String comunidad;
+
+    @Column(name = "username", nullable = false, length = 30)
+    private String username;
+
+    @Column(name = "password", nullable = false, length = 20)
+    private String password;
+
+    @ManyToMany(fetch = FetchType.LAZY)
+    @JoinTable(
+            name = "usuarios_roles",
+            joinColumns = @JoinColumn(name = "usuarios_id_usuario", referencedColumnName = "id_usuario"),
+            inverseJoinColumns = @JoinColumn(name = "roles_id_rol", referencedColumnName = "id_rol")
+    )
+    private Set<Roles> roles = new HashSet<>();
+
+
+    public Usuarios() {}
+
+    public Usuarios(Integer idUsuario, String nombre, String dni, String correo, String telefono, String direccion, String comunidad, String username, String password) {
+        this.idUsuario = idUsuario;
+        this.nombre = nombre;
+        this.dni = dni;
+        this.correo = correo;
+        this.telefono = telefono;
+        this.direccion = direccion;
+        this.comunidad = comunidad;
+        this.username = username;
+        this.password = password;
+    }
+
+    public Integer getIdUsuario() {
+        return idUsuario;
+    }
+
+    public void setIdUsuario(Integer idUsuario) {
+        this.idUsuario = idUsuario;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getDni() {
+        return dni;
+    }
+
+    public void setDni(String dni) {
+        this.dni = dni;
+    }
+
+    public String getCorreo() {
+        return correo;
+    }
+
+    public void setCorreo(String correo) {
+        this.correo = correo;
+    }
+
+    public String getTelefono() {
+        return telefono;
+    }
+
+    public void setTelefono(String telefono) {
+        this.telefono = telefono;
+    }
+
+    public String getDireccion() {
+        return direccion;
+    }
+
+    public void setDireccion(String direccion) {
+        this.direccion = direccion;
+    }
+
+    public String getComunidad() {
+        return comunidad;
+    }
+
+    public void setComunidad(String comunidad) {
+        this.comunidad = comunidad;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
 
