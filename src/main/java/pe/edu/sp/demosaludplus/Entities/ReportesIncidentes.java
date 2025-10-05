ae303523288cf8371c87c3a1402f16a9c4016601
Luis Falla <lucho75falla@gmail.com>
2025-10-05 11:49:36 -0500

    Agrego package Entities

--- src/main/java/pe/edu/sp/demosaludplus/Entities/ReportesIncidentes.java
+++ src/main/java/pe/edu/sp/demosaludplus/Entities/ReportesIncidentes.java
@@ -1,6 +1,7 @@
-package pe.edu.upc.demoSaludPlusNuevo.entities;
+package pe.edu.sp.demosaludplus.Entities;
 
 import jakarta.persistence.*;
+
 import java.time.LocalDate;
 import java.time.LocalTime;
 
